f73cf3dd50b72c44c4a7128ed319d474f73971ce
duqingbing <duqingbing@ghostcloud.cn>
2023-03-04 02:48:06 +0800

    json转化

--- utils/json.go
+++ utils/json.go
@@ -3,7 +3,7 @@ package utils
 import "encoding/json"
 
 // 任意数据转换成成json字符串
-func ToJson(data interface{}) (string, error) {
+func ToJson(data any) (string, error) {
 	bs, err := json.Marshal(data)
 	if err != nil {
 		return "", err
@@ -14,6 +14,6 @@ func ToJson(data interface{}) (string, error) {
 
 // json 字符串转换成结构体，数组，map等,
 // 对于传递的参数 objType, 必须传递指针进来
-func JsonToObj(data string, objType interface{}) error {
+func JsonToObj(data string, objType any) error {
 	return json.Unmarshal([]byte(data), &objType)
 }

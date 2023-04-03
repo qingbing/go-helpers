d619a4b2f901bb9933a84cdf83774696d100eb78
qingbing <780042175@qq.com>
2023-04-03 20:51:27 +0800

    map帮助函数

--- utils/map.go
+++ utils/map.go
@@ -1,15 +1,14 @@
 package utils
 
+import "reflect"
+
 // 判断一个map中是否完全包含另一个map
-func Contains(biggerMap, smallMap map[string]string) bool {
+func Contains[KT comparable, VT any](biggerMap, smallMap map[KT]VT) bool {
 	if len(biggerMap) == 0 || len(smallMap) == 0 {
 		return false
 	}
-	if len(biggerMap) < len(smallMap) {
-		return false
-	}
 	for sk, sv := range smallMap {
-		if bv, ok := biggerMap[sk]; !ok || sv != bv {
+		if bv, ok := biggerMap[sk]; !ok || !reflect.DeepEqual(sv, bv) {
 			return false
 		}
 	}
--- utils/map_test.go
+++ utils/map_test.go
@@ -1,118 +1,33 @@
 package utils
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/qingbing/go-helpers/debug"
 )
 
 func Test_Contains(t *testing.T) {
-	cs := []debug.Case{
-		{
-			Name: "all maps are nil",
-			Args: map[string]any{
-				"bmap": nil,
-				"smap": nil,
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "bigger map is nil",
-			Args: map[string]any{
-				"bmap": nil,
-				"smap": map[string]string{"name": "qingbing", "sex": "nan"},
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "smaller map is nil",
-			Args: map[string]any{
-				"bmap": map[string]string{"name": "qingbing", "sex": "nan"},
-				"smap": nil,
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "all maps's length is zero",
-			Args: map[string]any{
-				"bmap": map[string]string{},
-				"smap": map[string]string{},
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "bigger map's length is zero",
-			Args: map[string]any{
-				"bmap": map[string]string{},
-				"smap": map[string]string{"name": "qingbing", "sex": "nan"},
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "smaller map's length is zero",
-			Args: map[string]any{
-				"bmap": map[string]string{},
-				"smap": map[string]string{"name": "qingbing", "sex": "nan"},
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "bigger map does not contain all",
-			Args: map[string]any{
-				"bmap": map[string]string{"name": "qingbing"},
-				"smap": map[string]string{"name": "qingbing", "sex": "nan"},
-			},
-			Want: map[string]any{
-				"res": false,
-			},
-		},
-		{
-			Name: "bigger map is equal to smaller map",
-			Args: map[string]any{
-				"bmap": map[string]string{"name": "qingbing", "sex": "nan"},
-				"smap": map[string]string{"name": "qingbing", "sex": "nan"},
-			},
-			Want: map[string]any{
-				"res": true,
-			},
-		},
-		{
-			Name: "bigger map is bigger than smaller map",
-			Args: map[string]any{
-				"bmap": map[string]string{"name": "qingbing", "sex": "nan"},
-				"smap": map[string]string{"name": "qingbing"},
-			},
-			Want: map[string]any{
-				"res": true,
-			},
-		},
+	mp1 := map[string]string{
+		"name": "bing",
+		"sex":  "nan",
+		"age":  "18",
 	}
-	for _, c := range cs {
-		t.Run(c.Name, func(t *testing.T) {
-			bmap, ok := c.Args["bmap"].(map[string]string)
-			if !ok {
-				bmap = nil
-			}
-			smap, ok := c.Args["smap"].(map[string]string)
-			if !ok {
-				smap = nil
-			}
-			res := Contains(bmap, smap)
-			if res != c.Want["res"] {
-				t.Error(c.Errorf(res))
-			}
-		})
+	mp2 := map[string]string{
+		"name": "bing",
+		"sex":  "nan",
 	}
+
+	fmt.Println(Contains[string, string](mp1, mp2))
+	fmt.Println(Contains(mp1, mp2))
+
+	mp11 := map[string]int{
+		"name": 10,
+		"sex":  11,
+		"age":  11,
+	}
+	mp12 := map[string]int{
+		"name": 10,
+		"sex":  11,
+	}
+	fmt.Println(Contains[string, int](mp11, mp12))
+	fmt.Println(Contains(mp11, mp12))
 }
